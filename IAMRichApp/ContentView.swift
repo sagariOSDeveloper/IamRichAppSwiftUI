9bf8a30ac9e553f165d2328eb71aef0d06f5c6f9
Sagar Ali <sagar18.iosdeveloper@gmail.com>
2020-04-21 12:58:53 +0500

    Initial Commit

---
+++ IAMRichApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IAMRichApp
+//
+//  Created by Sagar Baloch on 21/04/2020.
+//  Copyright © 2020 Sagar Baloch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
